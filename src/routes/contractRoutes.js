5056f08dfcb66c840e5e26f64870b3d48cd3d7de
Aryamanraj <20cs3011@rgipt.ac.in>
2023-08-20 02:11:09 +0530

    Routes updates with bearer token

--- src/routes/contractRoutes.js
+++ src/routes/contractRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const contractController = require('../controllers/contractController');
+const { verifyUserToken, verifyAdminToken } = require('../middlewares/verificationMiddleware');
 
 // Routes for user data
-router.post('/set-user', contractController.setUser);
+router.post('/set-user', verifyStaticToken, contractController.setUser);
 router.get('/get-user/:rollNo', contractController.getUserByRollNo);
 router.get('/all-users-status', contractController.getAllUsersWithStatus);
 router.get('/is-approved/:rollNo', contractController.isUserApproved);
-router.delete('/delete-user/:rollNo', contractController.deleteUser);
+router.delete('/delete-user/:rollNo', verifyStaticToken, contractController.deleteUser);
 
 // Admin related routes
-router.put('/add-admin', contractController.addAdmin);
-router.put('/remove-admin', contractController.removeAdmin);
-router.put('/set-approval', contractController.setApprovalStatus);
+router.put('/add-admin', verifyAdminToken, contractController.addAdmin);
+router.put('/remove-admin', verifyAdminToken, contractController.removeAdmin);
+router.put('/set-approval', verifyStaticToken, contractController.setApprovalStatus);
 
 module.exports = router;
