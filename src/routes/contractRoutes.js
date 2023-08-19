44848b5095c278e46c6cd5f517b8edac0b9f83a8
Aryamanraj <20cs3011@rgipt.ac.in>
2023-08-20 02:45:21 +0530

    Bug fix @routes/contractRoutes.js

--- src/routes/contractRoutes.js
+++ src/routes/contractRoutes.js
@@ -4,15 +4,15 @@ const contractController = require('../controllers/contractController');
 const { verifyUserToken, verifyAdminToken } = require('../middlewares/verificationMiddleware');
 
 // Routes for user data
-router.post('/set-user', verifyStaticToken, contractController.setUser);
+router.post('/set-user', verifyUserToken, contractController.setUser);
 router.get('/get-user/:rollNo', contractController.getUserByRollNo);
 router.get('/all-users-status', contractController.getAllUsersWithStatus);
 router.get('/is-approved/:rollNo', contractController.isUserApproved);
-router.delete('/delete-user/:rollNo', verifyStaticToken, contractController.deleteUser);
+router.delete('/delete-user/:rollNo', verifyUserToken, contractController.deleteUser);
 
 // Admin related routes
 router.put('/add-admin', verifyAdminToken, contractController.addAdmin);
 router.put('/remove-admin', verifyAdminToken, contractController.removeAdmin);
-router.put('/set-approval', verifyStaticToken, contractController.setApprovalStatus);
+router.put('/set-approval', verifyUserToken, contractController.setApprovalStatus);
 
 module.exports = router;
