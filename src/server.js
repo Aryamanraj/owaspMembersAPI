bf5954e4f191c1973cf2e2de2c199384e0c3cb5a
Aryamanraj <20cs3011@rgipt.ac.in>
2023-08-21 15:09:33 +0530

    Cors updates

--- src/server.js
+++ src/server.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const contractRoutes = require('./routes/contractRoutes');
-const { errorHandler } = require('./middlewares/errorMiddleware');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const contractRoutes = require("./routes/contractRoutes");
+const { errorHandler } = require("./middlewares/errorMiddleware");
+const cors = require("cors");
 
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs'); // If your spec is in YAML format
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs"); // If your spec is in YAML format
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api', contractRoutes);  // All contract routes will be prefixed with '/api'
+const corsOptions = {
+  origin: /.*\.owasprgipt\.ac\.in$/, // regex pattern to allow all subdomains
+};
 
-const swaggerDocument = YAML.load('src/builds/swagger.yaml');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use("/api", contractRoutes); // All contract routes will be prefixed with '/api'
+
+const swaggerDocument = YAML.load("src/builds/swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+  console.log(`Server started on http://localhost:${port}`);
 });
 
 module.exports = app;
-
